a873c42962d0fb8677ff8a08ee27bed19c6b01f5
NoelStark <03noesta@skola.boras.se>
2022-02-19 18:23:31 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -1,210 +1,386 @@
-<?php include ("databaseConnect.php")
-
-
-?>
-
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" href="style.css">
-</head>
-<body>
-
-<div class = "grid">
-<ul>    
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+    <head>
+        <meta charset="utf-8">
+        <title>Responsive Navbar</title>
+        <meta name="viewport" 
+        content="width=device-width, initial-
+        scale=1.0">
+        <link rel="stylesheet" href="alphafamily.css">
+        <script src="https://kit.fontawesome.com/32e05b2de2.js"></script>
+    </head>
+    <body>
+        <nav class="nav-area">
+            <div class="menu-wrapper">
+            <input type="checkbox" id="check">
         
-        <form method="post" onsubmit = "return func2()">
+            <label for="check" class="checkbtn">
+                <i class="fas fa-bars"></i>
+            </label>
 
-    <li>
-        <div class = "gridOne">
-
-
-
-<!-- <script language="JavaScript">
-const t = new Date();
-let time = t.getHours();
-time = time+ 1;
-TargetDate = time.toString();
-console.log(TargetDate);
-BackColor = "palegreen";
-ForeColor = "navy";
-CountActive = true;
-CountStepper = -1;
-LeadingZero = true;
-DisplayFormat = "%%D%% Days, %%H%% Hours, %%M%% Minutes, %%S%% Seconds.";
-FinishMessage = "It is finally here!";
-</script>
-<script language="JavaScript" src="https://rhashemian.github.io/js/countdown.js"></script> -->
-
-
-        <?php
-        /*
-        $time = time();
-        $event = $time + 1;
-        $countdown = round(($event - $time)/86400);
-        $hours = date("H");
-
-        $target = intval($hours) + 1;
-        $target = strval($target);
-        echo $target.":".date("i:s");
-        echo "<br>";
-        */
-        
-        ?>
-      <div id = "time"></div>
-        Name: <input type="text" name="usn"><br>  
-        Password: <input type="password" name="psw" id = "psw" >
-        
-        <div id = "box">
-
-    <h4>Password must contain the following:</h4>
-    <p id="lower" class="invalid">A <b>lowercase</b> letter</p>
-    <p id="upper" class="invalid">An <b>uppercase</b> letter</p>
-    <p id="number" class="invalid">A <b>number</b> </p>
-    <p id="special" class="invalid">A <b>special character</b> </p>
-</div>
+            <div class="navbar">
+                
+                <a href="index.php"><img src="bilder/nedladdning.png"></a>
+                
+            </div>
+            <ul>
+                <li><a href="#home">Home</a></li>
+                <li><a href="#about">Cases</a></li>
+                <li><a href="#games">Updates</a></li>
+                <li><a href="signup.php">Log in</a></li>
+                <li><a href="signup.php">Sign Up</a></li>
+                        </div>
+                    </div>
+                
+            </ul>
         </div>
-    </li>
-    <li>
-        <div  class = "gridTwo">
+        </nav>
         
-        
-        
-        <input type="submit" value="Login" formaction="login.php">
+        <section id="home">
+            <h1>CSGO</h1>
+        </section>
 
-        <input type="submit" value="Sign up" formaction="signup.php">
+        <section id="about">
+        <h1>Cases</h1>
+        <br>
         
-        
-      
-        
+        <div class="container">
+        <div class="case1">
+            <h2>The Dreams & Nightmare Case</h2>
+            <div class="mySlides fade">
+                <div class="numbertext">1/17</div>
+                <img src="bilder/ak47.png">
+
+                <div class="red">AK47 | Nightwish</div>
+                
+            </div>
+            <div class="mySlides fade">
+                <div class="numbertext">2/17</div>
+                <img src="bilder/mp9.png">
+
+                <div class="red">MP9 | Starlight Protector</div>
+                
+            </div>
+
+            <div class="mySlides fade">
+                <div class="numbertext">3/17</div>
+                <img src="bilder/dual.png">
+
+                <div class="pink">Dual Berettas | Melondrama</div>
+                
+            </div>
+
+            <div class="mySlides fade">
+                <div class="numbertext">4/17</div>
+                <img src="bilder/famas.png">
+
+                <div class="pink">Famas | Rapid Eye Movement</div>
+                
+            </div>
+
+            <div class="mySlides fade">
+                <div class="numbertext">5/17</div>
+                <img src="bilder/mp7.png">
+
+                <div class="pink">MP7 | Abyssal Apparition</div>
+                
+            </div>
+
+            <div class="mySlides fade">
+                <div class="numbertext">6/17</div>
+                <img src="bilder/m4a1.png">
+
+                <div class="purple">M4A1-S | Night Terror</div>
+                
+            </div>
+
+            <div class="mySlides fade">
+                <div class="numbertext">7/17</div>
+                <img src="bilder/usp.png">
+
+                <div class="purple">USP-S | Ticket to Hell</div>
+                
+            </div>
+
+            <div class="mySlides fade">
+                <div class="numbertext">8/17</div>
+                <img src="bilder/pp-bizon.png">
+
+                <div class="purple">PP-Bizon | Space Cat</div>
+                
+            </div>
+
+            <div class="mySlides fade">
+                <div class="numbertext">9/17</div>
+                <img src="bilder/g3sg1.png">
+
+                <div class="purple">G3SG1 | Dream Glade</div>
+                
+            </div>
+
+            <div class="mySlides fade">
+                <div class="numbertext">10/17</div>
+                <img src="bilder/xm.png">
+
+                <div class="purple">XM1014 | Zombie Offensive</div>
+                
+            </div>
+
+            <div class="mySlides fade">
+                <div class="numbertext">11/17</div>
+                <img src="bilder/mac-10.png">
+
+                <div class="blue">MAC-10 | Ensnared</div>
+                
+            </div>
+
+            <div class="mySlides fade">
+                <div class="numbertext">12/17</div>
+                <img src="bilder/fiveseven.png">
+
+                <div class="blue">Five-SeveN | Scrawl</div>
+                
+            </div>
+
+            <div class="mySlides fade">
+                <div class="numbertext">13/17</div>
+                <img src="bilder/mp5-sd.png">
+
+                <div class="blue">MP5-SD | Necro Jr.</div>
+                
+            </div>
+
+            <div class="mySlides fade">
+                <div class="numbertext">14/17</div>
+                <img src="bilder/sawed-off.png">
+
+                <div class="blue">Sawed-Off | Spirit Board</div>
+                
+            </div>
+
+            <div class="mySlides fade">
+                <div class="numbertext">15/17</div>
+                <img src="bilder/p2000.png">
+
+                <div class="blue">P2000 | Lifted Spirits</div>
+                
+            </div>
+
+            <div class="mySlides fade">
+                <div class="numbertext">16/17</div>
+                <img src="bilder/mag7.png">
+
+                <div class="blue">MAG-7 | Foresight</div>
+                
+            </div>
+
+            <div class="mySlides fade">
+                <div class="numbertext">17/17</div>
+                <img src="bilder/scar2.png">
+
+                <div class="blue">SCAR-20 | Poultrygeist</div>
+                
+            </div>
+
+
+            <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
+            <a class="next" onclick="plusSlides(1)">&#10095;</a>
         </div>
-    </li>
-        
-    </form>
-        </ul>
-    
+            <div class="case2">
+                <h2>Riptide</h2>
+                <a href="#about"><img src="bilder/riptideCase.png" alt=""></a>
+            </div>
+
+            <div class="case3">
+                <h2>Snakebite</h2>
+                <a href="#about"><img src="bilder/snakebite.png" alt=""></a>
+            </div>
+
+            <div class="case4">
+                <h2>Broken Fang</h2>
+                <a href="#about"><img src="bilder/brokenfang.png" alt=""></a>
+            </div>
+
+            
+
         </div>
+
+        </section>
+
+        <section id="games">
+
+            <div class="container">
+                
+                <h1>Release Notes for 2/17/2022</h1>
+                <h4>2022.01.17 - CS</h4>
+                <div class="text" style="height:180px;">
+                    <h2>[General]</h2>
+                    <div id="a">
+                    <p>
+                    – Fixed Hammer crash on startup when not logged into Steam. <br>
+                    – Fixed input delay after closing radial menu.  
+                    </p>
+                   
+                </div>
+                
+                </div>
+
+                <div class="text" style="height: 180px;">
+                    <h2>[Steam Input]</h2>
+                    <div id="b">
+                        <p>
+                        – FlickStick mode is now enabled in Options > Controller, as opposed to the SteamInput Configurator. 
+                        Bind your Right Stick (or preferred aiming stick) to “Joystick Camera” in the configurator. <br>
+                        – Fixed input delay after closing radial menu. <br>
+                        – Fixed SteamInput ActionSet being set to “MenuControls” when re-focusing on window. <br>
+                        – Refinements to FlickStick behavior.
+                        </p>
+                </div>
+
+            </div>
+
+            <div class="text" style="height: 180px;">
+                <h2>[UI]</h2>
+                <div id="c">
+                    <p>
+                    – Tooltips to help clarify console controller settings. <br>
+                    – Various fixes to sliders.
+                </p>
+            </div>
+
+        
+            </div>
+            <div class="text" style="height: 180px;">
+                <h2>[Linux]</h2>
+                <div id="d">
+                    <p>
+                    – Added experimental Vulkan support. Add -vulkan to your command line to enable Vulkan.
+                </p>
+            </div>
+
+
+
+          <!--
+            <h1>CSGO</h1>
+            
+            <main>
+            
+            <input id="toggle" type="checkbox">
+            
+                <img src="ak47.png" alt="">
+                <h2>Rifles</h2>
+
+            <label for="toggle" >Click to Expand</label>
+            
+            <div id="offer">
+                
+                <p><img src="ak47.png" alt="" class="pic"> AK47</p>
+                <br>
+                <p><img src="m4a1.png" alt="" class="pic"> M4A1-S</p>
+                <br>
+                <p><img src="m4a4.png" alt="" class="pic">M4A4</p>
+                <br>
+                Galil
+                <br>
+                
+               
+            </div>
+            
+
+        
+        </main>
+-->
+        </section>
+
+        <script src="alpha.js">
+
+
+
+     /* function changec() { Clan 1
+       /*var xDiv = document.getElementById('box');
+       
+       if (xDiv.style.height == ''){
+           xDiv.style.height = '530px'
+           setTimeout(function(){document.getElementById('text-1').style.opacity = '1';}, 360)
+           document.getElementById('text-1').style.display = 'block';
+       }
+       /*
+         else 
+       {
+        document.getElementById('text-1').style.opacity = '0';
+        document.getElementById('text-1').style.display = 'none';
+           xDiv.style.height = ''
+        } 
+        
+        
+        }
+        
+    function changed(){/*Clan 2
+        var xdiv = document.getElementById('box2');
+
+        if (xdiv.style.height == ''){
+           xdiv.style.height = '530px';
+           setTimeout(function(){document.getElementById('text-2').style.opacity = '1';}, 360)
+           document.getElementById('text-2').style.display = 'block';
+        }
+       else 
+       {
+        document.getElementById('text-2').style.opacity = '0';
+        document.getElementById('text-2').style.display = 'none';
+           xdiv.style.height = ''
+        } 
+    }
+
+    function changee(){ /*Clan 3
+        var xdiv = document.getElementById('box3');
+
+        if (xdiv.style.height == ''){
+           xdiv.style.height = '530px';
+           setTimeout(function(){document.getElementById('text-3').style.opacity = '1';}, 360)
+           document.getElementById('text-3').style.display = 'block';
+        }
+       else 
+       {
+        document.getElementById('text-3').style.opacity = '0';
+        document.getElementById('text-3').style.display = 'none';
+           xdiv.style.height = ''
+        } 
+    }
+
+    function changef(){ /*Clan 4*
+        var xdiv = document.getElementById('box4');
+
+        if (xdiv.style.height == ''){
+           xdiv.style.height = '530px';
+           setTimeout(function(){document.getElementById('text-4').style.opacity = '1';}, 360)
+           document.getElementById('text-4').style.display = 'block';
+        }
+       else 
+       {
+        document.getElementById('text-4').style.opacity = '0';
+        document.getElementById('text-4').style.display = 'none';
+           xdiv.style.height = ''
+        } 
+    }
+
+    function changeg(){
+        var xdiv = document.getElementById('box5');
+
+        if (xdiv.style.height == ''){
+           xdiv.style.height = '530px';
+           setTimeout(function(){document.getElementById('text-5').style.opacity = '1';}, 360)
+           document.getElementById('text-5').style.display = 'block';
+        }
+       else 
+       {
+        document.getElementById('text-5').style.opacity = '0';
+        document.getElementById('text-5').style.display = 'none';
+           xdiv.style.height = ''
+        } 
+    }
+    */
     
-<script>
 
-function updateClock() {
-  var tid = new Date(); // current date
-    klockan = tid.getHours() + ':' + tid.getMinutes() + ":" + tid.getSeconds(), // again, you get the idea
-
-
-    // set the content of the element with the ID time to the formatted string
-    document.getElementById('time').innerHTML = [klockan].join(' / ');
-
-    // call this function again in 1000ms
-    setTimeout(updateClock, 1000);
-}
-updateClock(); // initial call
-
-// always checking if the element is clicked, if so, do alert('hello')
-const input = document.getElementById("psw");
-const lower = document.getElementById("lower");
-const lowerCase = /[a-z]/g;
-const upper = document.getElementById("upper");
-const upperCase = /[A-Z]/g;
-const number = document.getElementById("number");
-const num = /[0-9]/g;
-const special = document.getElementById("special");
-const specialCharacter = /[^a-zA-Z0-9\-\/]/;
-let checkLower = false;
-let checkUpper = false;
-let checkNumber = false;
-let checkSpecial = false;
-// When the user clicks on the password field, show the message box
-input.onfocus = function() {
-  document.getElementById("box").style.display = "block";
-}
-
-// When the user clicks outside of the password field, hide the message box
-input.onblur = function() {
-  setTimeout(function(){
-
-    document.getElementById("box").style.display = "none";
-  }, 200);
+        </script>
   
-}
-if(document.getElementById('btn1'))
-{
-   
-}
-else{
-
-
-input.onkeyup = function()
-{
-
-
-  if(input.value.match(lowerCase)) {  
-    lower.classList.remove("invalid");
-    lower.classList.add("valid");
-    checkLower = true;
-    
-  } else {
-    
-    lower.classList.remove("valid");
-    lower.classList.add("invalid");
-  }
-
-  //upper
-  if(input.value.match(upperCase))
-  {
-    upper.classList.remove("invalid");
-    upper.classList.add("valid");
-    checkUpper = true;
-  }
-  else
-  {
-    upper.classList.remove("valid");
-    upper.classList.add("invalid");
-  }
-
-  //number
-  if(input.value.match(num))
-  {
-    number.classList.remove("invalid");
-    number.classList.add("valid");
-    checkNumber = true;
-  }
-  else
-  {
-    number.classList.remove("valid");
-    number.classList.add("invalid");
-  }
-
-  //special
-
-  if(input.value.match(specialCharacter))
-  {
-    special.classList.remove("invalid");
-    special.classList.add("valid");
-    checkSpecial = true;
-  }
-  else
-  {
-    special.classList.remove("valid");
-    special.classList.add("invalid");
-  }
-  
-
-function func2(){
-  
-  if(checkLower == false || checkUpper == false || checkNumber == false || checkSpecial == false)
-  {
-    alert("Please fill out the requirements");
-    return false;
-  }
-}
-}
-}
-
-
-</script>
-</body>
-</html>
+    </body>
--- signup.php
+++ signup.php
@@ -1,146 +1,152 @@
-<?php
-use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\Exception;
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <link rel="stylesheet" href="signup.css">
+</head>
+<body>
+<form method="post" style="border: 1px solid #ccc" onsubmit = "return func2()">
+      <div class="container">
+        
+        <h1>Sign Up</h1>
+        <p>Please fill in this form to create an account.</p>
+        
+        <label for="usn" required><b>Username</b></label>
+        <input type="text" placeholder="Username" id="usn" name="usn" required />
+        <label for="email"><b>Email</b></label>
+        
+        <input type="text" placeholder="Enter Email" name="email" required />
+        <label for="psw"><b>Password</b></label>
+        <input
+          type="password"
+          placeholder="Enter Password"
+          name="psw"
+          id = "psw"
+          required
+        />
+        <label for="psw-repeat" ><b>Repeat Password</b></label>
+        <input type="password" placeholder="Repeat Password" name = "psw2"/>
 
-// var thing = 'harakiri-sushi';
-// var money = getServerMaxMoney(thing) *0.5;
-// nuke(thing);
-// while(true)
-// {
+      <div id = "box">
+        <h4>Password must contain the following:</h4>
+        <p id="lower" class="invalid">A <b>lowercase</b> letter</p>
+        <p id="upper" class="invalid">An <b>uppercase</b> letter</p>
+        <p id="number" class="invalid">A <b>number</b> </p>
+        <p id="special" class="invalid">A <b>special character</b> </p>
+      </div>
 
-// 	if(getServerMinSecurityLevel(thing) > 5)
-// 	{
-// 		brutessh(thing);
-// 		sqlinject(thing);
-// 		weaken(thing);
-// 	}
-// 	else if(getServerMaxMoney < 200000)
-// 	{
-// 		grow(thing);
-// 	}
-// 	else
-// 	{
-// 		hack(thing);
-// 	}
-// }
-require_once "vendor/autoload.php";
-?>
+        <div class="clearfix">
+          <button type="button" class="cancelbtn" formaction="index.php">Cancel</button>
+          <button type="submit" class="signupbtn" formaction="creatingAccount.php">Sign Up</button>
+        </div>
+      </div>
+    </form>
 
 
-<?php 
-
-session_start();  
-  
-$dbServername = "localhost";
-$dbUsername = "root";
-$dbPassword = "";
-$dbName = "login";
-
-$conn = mysqli_connect($dbServername, 
-$dbUsername, $dbPassword, $dbName);
-
-$usn = $_POST['usn'];
-$psw = $_POST['psw'];
-$_SESSION['user'] = $usn;
-
-//header( "refresh:3;url=index.php" );
-if(trim($usn) == '' || trim($psw) == '')
-{
-  
-    echo "Error: Username or Password invalid";
-    
+<script>
+const input = document.getElementById("psw");
+const lower = document.getElementById("lower");
+const lowerCase = /[a-z]/g;
+const upper = document.getElementById("upper");
+const upperCase = /[A-Z]/g;
+const number = document.getElementById("number");
+const num = /[0-9]/g;
+const special = document.getElementById("special");
+const specialCharacter = /[^a-zA-Z0-9\-\/]/;
+let checkLower = false;
+let checkUpper = false;
+let checkNumber = false;
+let checkSpecial = false;
+// When the user clicks on the password field, show the message box
+input.onfocus = function() {
+  document.getElementById("box").style.display = "block";
 }
-else
-{
-    $psw = password_hash($psw, PASSWORD_DEFAULT);
-    $query = mysqli_query($conn,"SELECT * FROM base1 WHERE username = '$usn'");
 
-  if($query->num_rows == 0) 
-  {
-  $sql = "INSERT INTO base1 (username, password)
-  VALUES ('$usn', '$psw')";
+// When the user clicks outside of the password field, hide the message box
+input.onblur = function() {
+  setTimeout(function(){
+
+    document.getElementById("box").style.display = "none";
+  }, 200);
   
-    
-    if (mysqli_query($conn, $sql)) 
-    {
-      $sql2 = "UPDATE `base1` SET time_of_creation= CURRENT_TIMESTAMP() WHERE username = '$usn'";
-      mysqli_query($conn, $sql2);
-      echo "Account successfully created!";
+}
+if(document.getElementById('btn1'))
+{
    
-    $code = bin2hex(random_bytes(16));
-    $_SESSION['code'] = $code;
-    $APP_URL = 'http://localhost/php/sql';
-    $activation_link = $APP_URL . "/verify.php?email=noelstark17@gmail.com&activation_code=$code";
-/*
-    $to_email = "noel.k.stark@gmail.com";
-    $subject = "Simple Email Test via PHP";
-    $body = "Hi, This is the code: ".$activation_link;
-    $headers = "From: stark.noel17@gmail.com";
+}
+else{
 
-/*
-      if (mail($to_email, $subject, $body, $headers)) 
-      {
-      echo "Email successfully sent to $to_email...";
-     
-      } 
 
-      else 
-      {
-      echo "Email sending failed...";
-      }
-    */
-    echo "New account created successfully";
-    echo "<br>";
+input.onkeyup = function()
+{
+
+
+  if(input.value.match(lowerCase)) {  
+    lower.classList.remove("invalid");
+    lower.classList.add("valid");
+    checkLower = true;
     
-
-
-     $mail = new PHPMailer(true);
-
-
-        $mail->IsSMTP();
-        $mail->Host = "smtp.gmail.com";
- 
-        $mail->SMTPAuth = true;
-        $mail->Username = 'stark.noel17@gmail.com';
-        $mail->Password = 'Noel123456789$';
-
-        $mail->addAddress("noel.k.stark@gmail.com");
-        
-        //Send HTML or Plain Text email
-        $mail->isHTML(true);
-        
-        $mail->Subject = "Subject Text";
-        $mail->Body = "Code:".$activation_link;
-        $mail->AltBody = "This is the plain text version of the email content";
-        
-        try 
-        {
-          
-            $mail->send();
-            echo "Sent yes";
-        } 
-        catch(Exception $e)
-        {
-            echo "Wrong".$e;
-        }
-        
-
-
-    $_SESSION[$usn] = 3;
-    } 
-
-    else 
-    {
-      echo "Error: " . $sql . "<br>" . mysqli_error($conn);
-    }
-      
-      //mysqli_close($conn);
+  } else {
+    
+    lower.classList.remove("valid");
+    lower.classList.add("invalid");
   }
 
+  //upper
+  if(input.value.match(upperCase))
+  {
+    upper.classList.remove("invalid");
+    upper.classList.add("valid");
+    checkUpper = true;
+  }
   else
   {
-    echo "Error: Username already exists";
+    upper.classList.remove("valid");
+    upper.classList.add("invalid");
   }
 
+  //number
+  if(input.value.match(num))
+  {
+    number.classList.remove("invalid");
+    number.classList.add("valid");
+    checkNumber = true;
+  }
+  else
+  {
+    number.classList.remove("valid");
+    number.classList.add("invalid");
+  }
 
+  //special
+
+  if(input.value.match(specialCharacter))
+  {
+    special.classList.remove("invalid");
+    special.classList.add("valid");
+    checkSpecial = true;
+  }
+  else
+  {
+    special.classList.remove("valid");
+    special.classList.add("invalid");
+  }
+  
+
+function func2(){
+  
+  if(checkLower == false || checkUpper == false || checkNumber == false || checkSpecial == false)
+  {
+    alert("Please fill out the requirements");
+    return false;
+  }
 }
+}
+}
+
+</script>
+</body>
+
+</html>
